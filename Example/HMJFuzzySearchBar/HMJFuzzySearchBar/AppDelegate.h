4402f0bec0c974e79efb0ad6d76dbfcd09195b20
贺梦洁 <2372621687@qq.com>
2016-09-05 11:01:39 +0800

    初始化项目并更改项目配置

---
+++ Example/HMJFuzzySearchBar/HMJFuzzySearchBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMJFuzzySearchBar
+//
+//  Created by MJHee on 16/9/5.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/HMJFuzzySearchBar/HMJFuzzySearchBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMJFuzzySearchBar
+//
+//  Created by MJHee on 16/9/5.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
