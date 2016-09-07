8487bb81b03c1c13c8c62f3e79f46fd6685e3cbb
贺梦洁 <2372621687@qq.com>
2016-09-07 14:15:04 +0800

    添加学校数据模型

---
+++ Example/HMJFuzzySearchBar/HMJFuzzySearchBar/HMJSchool.h
@@ -0,0 +1,16 @@
+//
+//  HMJSchool.h
+//  HMJSearchBar
+//
+//  Created by MJHee on 16/9/5.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMJSchool : NSObject
+
+@property (nonatomic, copy) NSString *schoolId;
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ Example/HMJFuzzySearchBar/HMJFuzzySearchBar/HMJSchool.m
@@ -0,0 +1,13 @@
+//
+//  HMJSchool.m
+//  HMJSearchBar
+//
+//  Created by MJHee on 16/9/5.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import "HMJSchool.h"
+
+@implementation HMJSchool
+
+@end
