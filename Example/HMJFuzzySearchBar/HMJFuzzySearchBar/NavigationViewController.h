baa71f36f78bb90f5f03363bb7f4e009d456654e
贺梦洁 <2372621687@qq.com>
2016-09-07 14:16:02 +0800

    添加导航控制器

---
+++ Example/HMJFuzzySearchBar/HMJFuzzySearchBar/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  HMJSearchBar
+//
+//  Created by MJHee on 16/9/5.
+//  Copyright © 2016年 MJHee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UINavigationController
+
+@end
